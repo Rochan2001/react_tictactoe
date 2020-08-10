874e8d19531757a457cb23c48f9ee62221d570fd
B.ROCHAN SINGH <bahadur.rochan@gmail.com>
2020-08-10 16:28:05 +0530

    Added state to the board

--- src/components/BoardComponent.js
+++ src/components/BoardComponent.js
@@ -2,29 +2,52 @@ import React from "react";
 import Square from "./SquareComponent";
 
 export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({
+      squares: squares,
+    });
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => {
+          this.handleClick(i);
+        }}
+      />
+    );
   }
   render() {
     return (
       <div>
         <h1>Tic Tac Toe</h1>
         <table>
-          <tr>
-            <td>{this.renderSquare(0)}</td>
-            <td class="vert">{this.renderSquare(1)}</td>
-            <td>{this.renderSquare(2)}</td>
-          </tr>
-          <tr>
-            <td class="hori">{this.renderSquare(3)}</td>
-            <td class="vert hori">{this.renderSquare(4)}</td>
-            <td class="hori">{this.renderSquare(5)}</td>
-          </tr>
-          <tr>
-            <td>{this.renderSquare(6)}</td>
-            <td class="vert">{this.renderSquare(7)}</td>
-            <td>{this.renderSquare(8)}</td>
-          </tr>
+          <tbody>
+            <tr>
+              <td>{this.renderSquare(0)}</td>
+              <td className="vert">{this.renderSquare(1)}</td>
+              <td>{this.renderSquare(2)}</td>
+            </tr>
+            <tr>
+              <td className="hori">{this.renderSquare(3)}</td>
+              <td className="vert hori">{this.renderSquare(4)}</td>
+              <td className="hori">{this.renderSquare(5)}</td>
+            </tr>
+            <tr>
+              <td>{this.renderSquare(6)}</td>
+              <td className="vert">{this.renderSquare(7)}</td>
+              <td>{this.renderSquare(8)}</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     );
